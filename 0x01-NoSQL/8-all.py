17048aada657a8e77ac272a7cf3d27034de20564
FourtyThree43 <shaqmwa@outlook.com>
2023-10-16 21:41:58 +0300

    I am the greatest javascript developer in the world.

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Module that lists all documents in a collection """
-from typing import Any, List
+from pymongo import collection
+from typing import List
 
 
-def list_all(mongo_collection:  ) -> List:
+def list_all(mongo_collection: collection.Collection) -> List:
     """ Function that lists all documents in a collection"""
-    pass
+    return list(mongo_collection.find())
