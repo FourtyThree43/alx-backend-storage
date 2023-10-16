f3880a0158bc58bafc0a8eca67a853f78a3ebe2a
FourtyThree43 <shaqmwa@outlook.com>
2023-10-16 07:28:57 -0700

    Task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that lists all documents in a collection """
+from typing import Any, List
+
+
+def list_all(mongo_collection:  ) -> List:
+    """ Function that lists all documents in a collection"""
+    pass
