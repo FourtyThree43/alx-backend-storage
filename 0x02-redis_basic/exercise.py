03a5c55eb300038034e78fef92c36ccf642dcfce
FourtyThree43 <shaqmwa@outlook.com>
2023-10-18 22:32:50 +0300

    commented out failing tests

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,9 +4,7 @@ Module for the Cache class
 """
 import redis
 import uuid
-from typing import Union  # Callable, Optional
-
-# from redis.commands.core import ResponseT
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -30,25 +28,26 @@ class Cache:
 
         return key
 
-    # def get(self, key: str, fn: Optional[Callable]) -> ResponseT:
-    #     """
-    #     Get the data
-    #     """
-    #     data = self._redis.get(key)
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Get the data
+        """
+        data = self._redis.get(key)
 
-    #     if data is not None and fn is not None:
-    #         return fn(data)
+        if data is not None and fn is not None:
+            return fn(data)
 
-    #     return data
+        return data
 
-    # def get_str(self, key: str) -> ResponseT:
-    #     """
-    #     Get the data as string
-    #     """
-    #     return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Get the data as string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-    # def get_int(self, key: str) -> ResponseT:
-    #     """
-    #     Get the data as int
-    #     """
-    #     return self.get(key, fn=int)
+    def get_int(self, key: str) -> int:
+        """
+        Get the data as int
+        """
+        return self.get(key, fn=int)
