0ce036992f066f6d8059e3ba88bbf4d3be807899
FourtyThree43 <shaqmwa@outlook.com>
2023-10-20 19:41:41 +0300

    add dirty scripts from the dark side of the universe

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -24,21 +24,22 @@ def main() -> None:
         "path": "/status"
     })
 
+    # Get the top 10 most present IPs
+    ip_counts = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
     print(f"{total_logs} logs")
     print("Methods:")
     for method, count in method_counts.items():
         print(f"\tmethod {method}: {count}")
     print(f"{status_check} status check")
 
-    print("\n")
     print("IPs:")
-    top_ips = collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ])
-    for ip in top_ips:
-        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+    for ip in ip_counts:
+        print(f"\t{ip['_id']}: {ip['count']}")
 
     client.close()
 
