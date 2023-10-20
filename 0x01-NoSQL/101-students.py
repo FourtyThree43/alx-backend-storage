3e2aad5ac56b15f59d0f59d78c4b7d1f75d1668f
FourtyThree43 <shaqmwa@outlook.com>
2023-10-20 19:33:16 +0300

    A fix I believe, not like I tested or anything

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""
+This module contains the function that returns all students
+sorted by average score
+"""
+from pymongo.collection import Collection
+from pymongo.command_cursor import CommandCursor
+
+
+def top_students(mongo_collection: Collection) -> CommandCursor:
+    """
+    Returns all students sorted by average score
+    """
+    students = mongo_collection.aggregate([{
+        "$project": {
+            "name": "$name",
+            "averageScore": {
+                "$avg": "$topics.score"
+            }
+        }
+    }, {
+        "$sort": {
+            "averageScore": -1
+        }
+    }])
+    return students
