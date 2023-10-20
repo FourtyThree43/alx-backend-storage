1b003e4d5848cdd3b8751ab4c79ab2d6b89e15cd
FourtyThree43 <shaqmwa@outlook.com>
2023-10-20 21:04:40 +0300

    remove debug all good

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python3
-"""A module with tools for request caching and tracking."""
+"""A module for caching data and tracking the number of accesses."""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 redis_store = redis.Redis()
-"""The module-level Redis instance.
-"""
 
 
-def data_cacher(method: Callable) -> Callable:
-    """Caches the output of fetched data."""
+def data_cacher(func: Callable[[str], str]) -> Callable[[str], str]:
+    """A decorator that caches data and tracks the number of accesses."""
 
-    @wraps(method)
-    def invoker(url) -> str:
+    @wraps(func)
+    def wrapper(url: str) -> str:
         """The wrapper function for caching the output."""
         redis_store.incr(f"count:{url}")
-        result = redis_store.get(f"result:{url}")
-        if result:
-            return result.decode("utf-8")
-        result = method(url)
-        redis_store.set(f"count:{url}", 0)
-        redis_store.setex(f"result:{url}", 10, result)
 
+        result = redis_store.get(f"result:{url}")
+        if result is not None:
+            return result.decode("utf-8")
+
+        result = func(url)
+        redis_store.setex(f"result:{url}", 10, result)
         return result
 
-    return invoker
+    return wrapper
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    """Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    """
+    """Fetch the content of a URL."""
     return requests.get(url).text
